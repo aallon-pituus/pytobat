0aa4bf853399c82844606171efe54dc94e43345a
aallon-pituus <119002731+aallon-pituus@users.noreply.github.com>
2024-08-09 18:41:36 +0300

    Update pytobat.py
    Added title for licenses

--- pytobat/pytobat.py
+++ pytobat/pytobat.py
@@ -118,6 +118,17 @@ Thank you for using this progam
     elif text_to_echo == "l":
         os.system("cls")
         print('''
+88888888ba           888888888888          88888888ba                           88           88                                                                         
+88      "8b               88               88      "8b                ,d        88           ""                                                                         
+88      ,8P               88               88      ,8P                88        88                                                                                      
+88aaaaaa8P'  8b       d8  88   ,adPPYba,   88aaaaaa8P'  ,adPPYYba,  MM88MMM     88           88   ,adPPYba,   ,adPPYba,  8b,dPPYba,   ,adPPYba,   ,adPPYba,  ,adPPYba,  
+88""""""'    `8b     d8'  88  a8"     "8a  88""""""8b,  ""     `Y8    88        88           88  a8"     ""  a8P_____88  88P'   `"8a  I8[    ""  a8P_____88  I8[    ""  
+88            `8b   d8'   88  8b       d8  88      `8b  ,adPPPPP88    88        88           88  8b          8PP"""""""  88       88   `"Y8ba,   8PP"""""""   `"Y8ba,   
+88             `8b,d8'    88  "8a,   ,a8"  88      a8P  88,    ,88    88,       88           88  "8a,   ,aa  "8b,   ,aa  88       88  aa    ]8I  "8b,   ,aa  aa    ]8I  
+88               Y88'     88   `"YbbdP"'   88888888P"   `"8bbdP"Y8    "Y888     88888888888  88   `"Ybbd8"'   `"Ybbd8"'  88       88  `"YbbdP"'   `"Ybbd8"'  `"YbbdP"'  
+                 d8'                                                                                                                                                    
+                d8'                                                                                                                                                     
+
               (MIT LICENSE)
 
     Copyright 2024 aallon-pituus (Github)
